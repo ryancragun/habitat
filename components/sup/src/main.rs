e520afeae9724700ca139bff8a996249b7437908
Matthew Peck <mpeck@chef.io>
2019-02-25 11:28:40 -0500

    reformat with nightly rustfmt
    Signed-off-by: Matthew Peck <mpeck@chef.io>

--- components/sup/src/main.rs
+++ components/sup/src/main.rs
@@ -39,46 +39,51 @@ extern crate time;
 extern crate tokio_core;
 extern crate url;
 
-use std::{
-    env,
-    io::{self, Write},
-    net::{Ipv4Addr, SocketAddr, ToSocketAddrs},
-    path::PathBuf,
-    process,
-    str::{self, FromStr},
-};
+use std::{env,
+          io::{self,
+               Write},
+          net::{Ipv4Addr,
+                SocketAddr,
+                ToSocketAddrs},
+          path::PathBuf,
+          process,
+          str::{self,
+                FromStr}};
 
-use crate::{
-    common::{
-        cli_defaults::GOSSIP_DEFAULT_PORT,
-        command::package::install::InstallSource,
-        ui::{Coloring, NONINTERACTIVE_ENVVAR, UI},
-    },
-    hcore::{
-        crypto::{self, default_cache_key_path, SymKey},
-        env as henv,
-        url::{bldr_url_from_env, default_bldr_url},
-        ChannelIdent,
-    },
-    launcher_client::{LauncherCli, ERR_NO_RETRY_EXCODE},
-    protocol::{
-        ctl::ServiceBindList,
-        types::{ApplicationEnvironment, BindingMode, ServiceBind, Topology, UpdateStrategy},
-    },
-};
+use crate::{common::{cli_defaults::GOSSIP_DEFAULT_PORT,
+                     command::package::install::InstallSource,
+                     ui::{Coloring,
+                          NONINTERACTIVE_ENVVAR,
+                          UI}},
+            hcore::{crypto::{self,
+                             default_cache_key_path,
+                             SymKey},
+                    env as henv,
+                    url::{bldr_url_from_env,
+                          default_bldr_url},
+                    ChannelIdent},
+            launcher_client::{LauncherCli,
+                              ERR_NO_RETRY_EXCODE},
+            protocol::{ctl::ServiceBindList,
+                       types::{ApplicationEnvironment,
+                               BindingMode,
+                               ServiceBind,
+                               Topology,
+                               UpdateStrategy}}};
 use clap::ArgMatches;
 use habitat_common as common;
 #[cfg(windows)]
 use hcore::crypto::dpapi::encrypt;
 
-use crate::sup::{
-    cli::cli,
-    command,
-    error::{Error, Result, SupError},
-    feat,
-    manager::{Manager, ManagerConfig},
-    util,
-};
+use crate::sup::{cli::cli,
+                 command,
+                 error::{Error,
+                         Result,
+                         SupError},
+                 feat,
+                 manager::{Manager,
+                           ManagerConfig},
+                 util};
 
 #[cfg(test)]
 use tempfile::TempDir;
@@ -157,9 +162,7 @@ fn start() -> Result<()> {
     }
 }
 
-fn sub_bash() -> Result<()> {
-    command::shell::bash()
-}
+fn sub_bash() -> Result<()> { command::shell::bash() }
 
 fn sub_run(m: &ArgMatches, launcher: LauncherCli) -> Result<()> {
     set_supervisor_logging_options(m);
@@ -203,9 +206,7 @@ fn sub_run(m: &ArgMatches, launcher: LauncherCli) -> Result<()> {
     manager.run(svc)
 }
 
-fn sub_sh() -> Result<()> {
-    command::shell::sh()
-}
+fn sub_sh() -> Result<()> { command::shell::sh() }
 
 fn sub_term() -> Result<()> {
     // We were generating a ManagerConfig from matches here, but 'hab sup term' takes no options.
@@ -361,9 +362,7 @@ fn bldr_url_from_input(m: &ArgMatches) -> Option<String> {
 
 /// Resolve a channel. Taken from CLI args, or (failing that), a
 /// default value.
-fn channel(matches: &ArgMatches) -> ChannelIdent {
-    channel_from_input(matches).unwrap_or_default()
-}
+fn channel(matches: &ArgMatches) -> ChannelIdent { channel_from_input(matches).unwrap_or_default() }
 
 /// A channel name, but *only* if the user specified via CLI args.
 fn channel_from_input(m: &ArgMatches) -> Option<ChannelIdent> {
@@ -442,9 +441,7 @@ fn get_password_from_input(m: &ArgMatches) -> Result<Option<String>> {
 }
 
 #[cfg(any(target_os = "linux", target_os = "macos"))]
-fn get_password_from_input(_m: &ArgMatches) -> Result<Option<String>> {
-    Ok(None)
-}
+fn get_password_from_input(_m: &ArgMatches) -> Result<Option<String>> { Ok(None) }
 
 fn enable_features_from_env() {
     let features = vec![
@@ -536,10 +533,10 @@ fn update_svc_load_from_input(m: &ArgMatches, msg: &mut protocol::ctl::SvcLoad) 
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::{
-        common::{locked_env_var, types::ListenCtlAddr},
-        sup::{config::GossipListenAddr, http_gateway},
-    };
+    use crate::{common::{locked_env_var,
+                         types::ListenCtlAddr},
+                sup::{config::GossipListenAddr,
+                      http_gateway}};
 
     mod manager_config {
 
@@ -553,9 +550,7 @@ mod test {
             config_from_cmd_vec(cmd_vec)
         }
 
-        fn cmd_vec_from_cmd_str(cmd: &str) -> Vec<&str> {
-            Vec::from_iter(cmd.split_whitespace())
-        }
+        fn cmd_vec_from_cmd_str(cmd: &str) -> Vec<&str> { Vec::from_iter(cmd.split_whitespace()) }
 
         fn config_from_cmd_vec(cmd_vec: Vec<&str>) -> ManagerConfig {
             let matches = cli()
